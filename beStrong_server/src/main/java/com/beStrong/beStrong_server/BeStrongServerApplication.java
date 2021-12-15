92a644901dc85a86f34609ddc47fd0fe9c239e75
pedromonteiro01 <pmapm@ua.pt>
2021-12-15 17:07:40 +0000

    upload docker files and initial spring boot files

---
+++ beStrong_server/src/main/java/com/beStrong/beStrong_server/BeStrongServerApplication.java
@@ -0,0 +1,13 @@
+package com.beStrong.beStrong_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStrongServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeStrongServerApplication.class, args);
+	}
+
+}
---
+++ beStrong_server/src/test/java/com/beStrong/beStrong_server/BeStrongServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beStrong.beStrong_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStrongServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
