e31358a7e780e23b49e36aa85abbb33db4a3b5d7
rezeett <edu_fernandes_2001@live.com.pt>
2022-01-13 16:01:50 +0000

    updated project

--- beStrong_server/src/main/java/com/beStrong/beStrong_server/controller/ClientController.java
+++ beStrong_server/src/main/java/com/beStrong/beStrong_server/controller/ClientController.java
@@ -1,16 +1,21 @@
 package com.beStrong.beStrong_server.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beStrong.beStrong_server.model.Client;
+import com.beStrong.beStrong_server.model.User;
 import com.beStrong.beStrong_server.service.ClientService;
 import com.beStrong.beStrong_server.repository.*;
 
@@ -20,6 +25,7 @@ public class ClientController {
 
     @Autowired
     private ClientService clientService;
+    private UserRepository userRepository;
 
     @GetMapping
     @ResponseBody
---
+++ beStrong_server/src/main/resources/static/js/sign-up.js
@@ -0,0 +1,47 @@
+async function getData(url1, user, pw){
+    const response1 = await fetch(url1);
+    var data1 = await response1.json();
+    console.log(data1);
+    
+    verify_login(data1, user, pw);
+}
+
+function login(){
+    console.log("control");
+    var username=document.getElementById("input-email").value;
+    var password=document.getElementById("input-password").value;
+
+    if(username=="" || password==""){
+        alert("You need to fill all fields!");
+        return;
+    }
+
+    console.log("user: ", username);
+    console.log("pass: ", password);
+
+    getData("http://172.18.0.9:8081/clients", username, password);
+
+}
+
+function verify_login(data, user, pw){
+    var list;
+    if (data.length <= 1)
+        list = data;
+    else
+        list = data.list;
+    for (let i = 0; i < data.length; i++) {
+        let cls = data[i];
+        if (cls.email == user && cls.password==pw){
+            console.log("user:",cls.email);
+            localStorage.setItem("loggedIn", 1);
+            localStorage.setItem("Email", cls.email);
+            localStorage.setItem("Username", cls.name);
+            localStorage.setItem("Phonenumber", cls.phone);
+            localStorage.setItem("Password", pw);
+            window.location.href="index.html";
+            return;
+        }
+    }
+    alert("Wrong credentials!");
+    return;
+}
