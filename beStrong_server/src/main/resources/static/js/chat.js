cf1f8b1c28df1d84e245c9a3320b20532d95bf25
pedromonteiro01 <pmapm@ua.pt>
2022-01-19 19:16:48 +0000

    finish chat

--- beStrong_server/src/main/resources/static/js/chat.js
+++ beStrong_server/src/main/resources/static/js/chat.js
@@ -20,12 +20,12 @@ function connect() {
 }
 
 function onConnected() {
-    //stompClient.subscribe('/topic/public', onMessageReceived);
+    stompClient.subscribe('/topic/public', onMessageReceived);
 
-    stompClient.send("/app/chat.register",
-        {},
-        JSON.stringify({sender: username, type: 'JOIN'})
-    )
+    //stompClient.send("/app/chat.register",
+     //   {},
+     //   JSON.stringify({sender: username, type: 'JOIN'})
+    //)
 }
 
 function onError(error) {
@@ -34,29 +34,39 @@ function onError(error) {
 
 function onMessageReceived(payload) {
     var message = JSON.parse(payload.body);
-    console.log(message);
+    //console.log(message);
 
-    var messageElement = document.createElement('li');
+    if (message.type === 'CHAT'){
+        //console.log("chat message incoming");
+        var img = '';
+        if (localStorage.getItem("isTrainer") == 0) {
+            //console.log("im trainer");
+            img = 'img/pt.png';
+        } else {
+            //console.log("im client");
+            img = 'img/user.png';
+        }
 
-    if(message.type === 'JOIN') {
-        messageElement.classList.add('event-message');
-        message.content = message.sender + ' joined!';
-    } else if (message.type === 'LEAVE') {
-        messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
-    } else if (message.type === 'CHAT'){
-        console.log("chat message incoming")
-        //var avatarElement = document.createElement('i');
-        //var avatarText = document.createTextNode(message.sender[0]);
-        //avatarElement.appendChild(avatarText);
-        //avatarElement.style['background-color'] = getAvatarColor(message.sender);
-
-        //messageElement.appendChild(avatarElement);
-
-        //var usernameElement = document.createElement('span');
-        //var usernameText = document.createTextNode(message.sender);
-        //usernameElement.appendChild(usernameText);
-        //messageElement.appendChild(usernameElement);
+        var msgDisplay = ``;
+        if (message.sender == username){
+            msgDisplay+=`   <div class="chat outgoing">
+                                <div class="details">
+                                    <p>${message.content}</p>
+                                </div>
+                            </div>
+                        `;
+        } else {
+            msgDisplay+=`   <div class="chat incoming">
+                                <img src="${img}" alt="">
+                                <div class="details">
+                                    <p>${message.content}</p>
+                                </div>
+                            </div>
+            `;
+        }
+        document.getElementById("chat-box").innerHTML += msgDisplay;
+        var chat_box = document.getElementById("chat-box");
+        chat_box.scrollTop = chat_box.scrollHeight;
     } else {
         console.log("nothing...")
     }
@@ -75,16 +85,5 @@ function send() {
 
         stompClient.send("/app/chat.send", {}, JSON.stringify(chatMessage));
         messageInput.value = '';
-
-        var msgDisplay = ``;
-        msgDisplay+=`   <div class="chat outgoing">
-                            <div class="details">
-                                <p>${chatMessage.content}</p>
-                            </div>
-                        </div>
-                    `;
-        document.getElementById("chat-box").innerHTML += msgDisplay;
-        var chat_box = document.getElementById("chat-box");
-        chat_box.scrollTop = chat_box.scrollHeight;
     }
 }
